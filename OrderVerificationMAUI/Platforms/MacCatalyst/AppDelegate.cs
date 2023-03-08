e254cb089acaa7153e14f9caafa0d5cfdb3ee44e
suawilhelm <60698745+suawilhelm@users.noreply.github.com>
2023-03-08 14:45:20 +0100

    Working solution
    -added MAUI framework

---
+++ OrderVerificationMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OrderVerificationMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
