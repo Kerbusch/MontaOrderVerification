1d690cd523a27ff51d365f408d22e8cefb780597
Jarno-2003 <57068326+Jarno-2003@users.noreply.github.com>
2023-03-15 10:45:41 +0100

    basic sku invoer scherm

--- OrderVerificationMAUI/MainPage.xaml.cs
+++ OrderVerificationMAUI/MainPage.xaml.cs
@@ -2,23 +2,83 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	bool input = false;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    /*private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		if (SkuEntry.Text != "" && !input)
+        {
+			SkuEntry.Text = SkuEntry.Text.Replace("Sku invoeren", "");
+			input = true;
+        }
+		/*else if (SkuEntry.Text == "")
+		{
+			SkuEntry.Text = "Sku invoeren";
+			input = false;
+        }
+        SemanticScreenReader.Announce(SkuEntry.Text);
+    }*/
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void Clicked_1(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "1";
+    }
+    private void Clicked_2(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "2";
+    }
+    private void Clicked_3(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "3";
+    }
+    private void Clicked_4(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "4";
+    }
+    private void Clicked_5(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "5";
+    }
+    private void Clicked_6(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "6";
+    }
+    private void Clicked_7(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "7";
+    }
+    private void Clicked_8(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "8";
+    }
+    private void Clicked_9(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "9";
+    }
+    private void Clicked_0(object sender, EventArgs e)
+    {
+        SkuEntry.Text += "0";
+    }
+    private void Clicked_delete(object sender, EventArgs e)
+    {
+        SkuEntry.Text = "";
+    }
+    private void Clicked_back(object sender, EventArgs e)
+    {
+        if (SkuEntry.Text != "")
+        {
+            SkuEntry.Text = SkuEntry.Text.Remove(SkuEntry.Text.Length - 1);
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void Clicked_enter(object sender, EventArgs e)
+    {
+        SkuEntry.Text = "To next page";
+    }
 }
 
