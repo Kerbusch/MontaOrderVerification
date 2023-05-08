f432dfb4c8dace6111d0ede88b8ad32b40b4ed99
Kerbusch <daankerbusch@gmail.com>
2023-05-08 15:28:27 +0200

    Created the OutboundProcess class and console app - Added a console app to the solution for the outbound process. Places the OrderLogic classes here to be used. This may be moved in the future if this implementation needs te be used by different projects. - Added a todo to SkuFromImageRpc.cs that will be fixed in #137

---
+++ OrderVerificationMAUI/OutboundConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OrderLogic;
+
+//Class for running the class above
+class Run {
+	static async Task Main() {
+		//Add order
+		// using OrderData order_data = new OrderData("../../../file.json");
+		// order_data.addOrder(new long[]{3,4,5,6});
+		// order_data.addOrder(new long[]{1,1,1,2});
+		// return;
+		
+		//Run the outbound process in a single loop
+		using var outbound_process = new OutboundProcess(
+			"20.13.19.141",
+			"python_test_user",
+			"jedis"
+		);
+		
+		// outbound_process.runProcessOnce();
+		outbound_process.startLoop();
+	}
+}
