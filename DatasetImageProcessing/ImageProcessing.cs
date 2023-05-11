17224f622b8255acb7cd1edbb0b23911fad8d5b2
Jarno-2003 <57068326+Jarno-2003@users.noreply.github.com>
2023-05-11 14:11:20 +0200

    Revert "added commands"
    This reverts commit e0c4bac7609b178b4a9d98562095b81dc894f09e.

--- DatasetImageProcessing/ImageProcessing.cs
+++ DatasetImageProcessing/ImageProcessing.cs
@@ -8,26 +8,27 @@ using OpenCvSharp;
 
 namespace DatasetImageProcessing
 {
-    // Class to process images by changing the background to a color or to alpha (transparent)
     internal class ImageProcessing
     {
-        // Constructor
         public ImageProcessing() { }
 
-        // Function to change background color to the given RGB color
-        public Mat changeColor(Mat image, int[] RGB)
+        public Mat changeColor(Mat image, int[] RBG)
         {
             Mat new_mat = image;
-            for (int y = 0; y < image.Rows; ++y) {
-                for (int x = 0; x < image.Cols; ++x) {
+            for (int y = 0; y < image.Rows; ++y)
+            {
+                for (int x = 0; x < image.Cols; ++x)
+                {
                     //Console.WriteLine(y + ", " + x);
-                    if (image.At<Vec4b>(y, x)[3] == 0) {
-                        new_mat.At<Vec4b>(y, x)[0] = (byte)RGB[2];
-                        new_mat.At<Vec4b>(y, x)[1] = (byte)RGB[1];
-                        new_mat.At<Vec4b>(y, x)[2] = (byte)RGB[0];
+                    if (image.At<Vec4b>(y, x)[3] == 0)
+                    {
+                        new_mat.At<Vec4b>(y, x)[0] = (byte)RBG[2];
+                        new_mat.At<Vec4b>(y, x)[1] = (byte)RBG[1];
+                        new_mat.At<Vec4b>(y, x)[2] = (byte)RBG[0];
                         new_mat.At<Vec4b>(y, x)[3] = 0;
                     }
-                    else {
+                    else
+                    {
                         new_mat.At<Vec4b>(y, x) = image.At<Vec4b>(y, x);
                     }
                 }
@@ -36,19 +37,25 @@ namespace DatasetImageProcessing
             return new_mat;
         }
 
-        // Function to change background pixels to alpha (transparent)
         public Mat getAlphaMat(Mat image)
         {
             Mat input = new();
             OpenCvSharp.Size s = new OpenCvSharp.Size(3, 3);
             Cv2.GaussianBlur(image, input, s, 0);
+
             Cv2.CvtColor(input, input, ColorConversionCodes.BGR2BGRA);
+
             Mat[] channels = Cv2.Split(input);
+
+
             Cv2.Merge(channels, input);
 
-            for (int y = 0; y < input.Rows; ++y) {
-                for (int x = 0; x < input.Cols; ++x) {
-                    if (input.At<Vec4b>(y, x)[0] <= 100 && input.At<Vec4b>(y, x)[1] <= 100 && input.At<Vec4b>(y, x)[2] <= 100) {
+            for (int y = 0; y < input.Rows; ++y)
+            {
+                for (int x = 0; x < input.Cols; ++x)
+                {
+                    if (input.At<Vec4b>(y, x)[0] <= 100 && input.At<Vec4b>(y, x)[1] <= 100 && input.At<Vec4b>(y, x)[2] <= 100)
+                    {
                         input.At<Vec4b>(y, x)[3] = 0;
                     }
                 }
