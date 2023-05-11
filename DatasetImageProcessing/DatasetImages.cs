5d5978e9a07d25b208b414f8d8ca24a6aeba3e9f
Jarno-2003 <57068326+Jarno-2003@users.noreply.github.com>
2023-05-11 14:36:33 +0200

    added setup for DatasetImages and ImageLabeling

---
+++ DatasetImageProcessing/DatasetImages.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace DatasetImageProcessing
+{
+    internal class DatasetImages
+    {
+        // Constructor
+        public DatasetImages() { }
+
+        // 
+        public void saveImageToDatasetAndChangeColor(Mat picture, string path, string sku)
+        {
+        }
+
+        // 
+        public string getSkuFolderPath(string sku, string vendor, Mat picture)
+        {
+            return "";
+        }
+    }
+}
---
+++ DatasetImageProcessing/ImageLabeling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace DatasetImageProcessing
+{
+    internal class ImageLabeling
+    {
+        // Constructor
+        public ImageLabeling() { }
+
+        // 
+        public int[] getBoundingBox(Mat input)
+        {
+            return new int[] { 0, 0, 0 ,0 };
+        }
+
+        // 
+        public void createLabel(int[] bounding_box, Mat picture, string sku, string path)
+        {
+        }
+    }
+}
