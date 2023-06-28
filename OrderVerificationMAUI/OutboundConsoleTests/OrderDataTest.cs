527565cdbd42a285319e07fe2891fe1e8e1d286d
Kerbusch <daankerbusch@gmail.com>
2023-06-28 11:17:59 +0200

    OutboundConsole update -Added more feedback -Added some unit tests and made preparations for the rest

---
+++ OrderVerificationMAUI/OutboundConsoleTests/OrderDataTest.cs
@@ -0,0 +1,52 @@
+﻿namespace OutboundConsoleTests; 
+
+public class OrderDataTest {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void loadFromFileTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void saveToFileTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void getAllOrdersTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void tryGetOrderTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void setSkusInOrderTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void addOrderTest0() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void addOrderTest1() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void removeOrderTest() {
+        Assert.Warn("not implemented");
+    }
+    
+    [Test]
+    public void disposeTest() {
+        Assert.Warn("not implemented");
+    }
+}
